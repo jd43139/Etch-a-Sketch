7f8d2e3dbd520f95a6a9a7774e72b052fd609c58
jd43139 <109301496+jd43139@users.noreply.github.com>
2024-01-08 16:16:14 +0400

    iii

--- sketch/dd.js
+++ sketch/dd.js
@@ -2,17 +2,20 @@
 const button = document.querySelector("#add-grids")
 let frame = document.querySelector(".frame")
 let eraseButton = document.querySelector("#delete")
+let body = document.querySelector("body")
 let red = 100
 let green  = 100
 let blue =100
-
+let pixelNum =8
+let count2 = 0
 function createv_pixelss(){
      
-    let pixelNum =8
+    
     red = 100
     green = 100
     blue =100
     pixelNum = prompt("Enter a grid layout", pixelNum)
+    if(pixelNum>128)return alert("Not more than 128");
     for(let i =0; i<pixelNum; i++){
         let v_pixels = document.createElement("div")
       
@@ -37,8 +40,19 @@ function createv_pixelss(){
     
    h_pixel = document.querySelectorAll(".h-pixel")
    h_pixel.forEach((div)=> {
-        div.addEventListener("mouseover", gradedBlackHover)
-       
+        window.addEventListener("keydown", (event)=>{
+            if(event.key==="z"){
+                div.addEventListener("mouseover", gradedBlackHover)
+            }
+
+            
+        })
+        window.addEventListener("keyup", (event)=>{
+            if(event.key==="z"){
+                div.removeEventListener("mouseover", gradedBlackHover)
+            }
+            
+        })
         
         
     })
@@ -48,18 +62,6 @@ function createv_pixelss(){
    for(let i=0; i<v_pixels.length-pixelNum; i++){
     frame.removeChild(v_pixels[i])
    }
-
-   
-   
-    
-
-    
-   
-   
-   
-   
-   
-   
 }
 button.addEventListener("click", createv_pixelss)
 let count = 0
@@ -68,10 +70,15 @@ function gradedBlackHover(){
     
     let options = document.querySelectorAll("option")
     if(options[0].selected){
-        
+        count++
         red-=10
         green-=10
         blue-=10
+        if(count%10==0){
+            red =100
+            green = 100
+            blue = 100
+        }
     }else{
          red = Math.floor(Math.random()*256)
          green = Math.floor(Math.random()*256)
@@ -88,17 +95,18 @@ function gradedBlackHover(){
 
 
 
-
+function white(){
+    this.style.background = "white"
+}
 
 function erase(){
     red = 100
     green = 100
     blue = 100
     h_pixel = document.querySelectorAll(".h-pixel")
-    h_pixel.forEach((div)=> div.style.background="white")
+    h_pixel.forEach((div)=> div.style.background=body.style.background)
 }
 eraseButton.addEventListener("click", erase)
-
 
 
 
